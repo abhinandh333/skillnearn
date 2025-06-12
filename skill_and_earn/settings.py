c31bcd86d083f78b588526cdd6b8690e66420f56
Abhinandh R Nair <abhinandhrnair333@gmail.com>
2025-06-12 07:48:11 +0530

    Your commit message

--- skill_and_earn/settings.py
+++ skill_and_earn/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-t4qd!)n0gr8xa=@f#4n*ik_nzrfm#)=set$31u#x(so#ys3n+p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
